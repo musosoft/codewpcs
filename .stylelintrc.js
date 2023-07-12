633fdc49793c3ec2cd560aa06db3e439d9def9e3
Roman Kovac <musosoft@gmail.com>
2023-07-12 16:16:17 +0200

    Fix ignored CSS files not being linted

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-	extends: [ '@wordpress/stylelint-config/scss' ],
-	rules: {
-		// Your custom rules here, e.g.: 'no-descending-specificity': null
-	},
+    extends: [ '@wordpress/stylelint-config/scss' ],
+    rules: {
+        // Your custom rules here, e.g.: 'no-descending-specificity': null
+    },
+    overrides: [
+        {
+            files: [
+                '**/*.css',
+                '**/*.scss',
+            ],
+        },
+    ],
 };
